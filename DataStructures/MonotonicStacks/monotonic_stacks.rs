0c8fa7f6d77a55b7dfcd679667157414d4941e2f
Sarthak Mathur <mathursarthak2182@gmail.com>
2025-10-25 03:28:35 +0530

    Fix monotonic stack

--- DataStructures/MonotonicStacks/monotonic_stacks.rs
+++ DataStructures/MonotonicStacks/monotonic_stacks.rs
@@ -1,6 +1,11 @@
 pub mod monotonic_stacks {
     use std::collections::VecDeque;
 
+    /// `cmp` will be called like `cmp(curr_value, next_value)`
+    ///
+    /// If we want the next smaller element, then
+    ///
+    /// `cmp = |curr_value, next_value| -> next_value < curr_value`
     pub fn monotonic_stack_next<T, F: Fn(&T, &T) -> bool>(a: &[T], cmp: F) -> Vec<usize> {
         let n = a.len();
         let mut st = VecDeque::new();
@@ -8,9 +13,9 @@ pub mod monotonic_stacks {
         for i in (0..n).rev() {
             while let Some(&j) = st.back() {
                 if cmp(&a[i], &a[j]) {
-                    st.pop_back();
-                } else {
                     break;
+                } else {
+                    st.pop_back();
                 }
             }
             next[i] = st.back().copied().unwrap_or(n);
@@ -19,6 +24,11 @@ pub mod monotonic_stacks {
         next
     }
 
+    /// `cmp` will be called like `cmp(curr_value, prev_value)`
+    ///
+    /// If we want the previous smaller element, then
+    ///
+    /// `cmp = |curr_value, prev_value| -> prev_value < curr_value`
     pub fn monotonic_stack_prev<T, F: Fn(&T, &T) -> bool>(a: &[T], cmp: F) -> Vec<isize> {
         let n = a.len();
         let mut st: VecDeque<isize> = VecDeque::new();
@@ -26,9 +36,9 @@ pub mod monotonic_stacks {
         for i in 0..n {
             while let Some(&j) = st.back() {
                 if cmp(&a[i], &a[j as usize]) {
-                    st.pop_back();
-                } else {
                     break;
+                } else {
+                    st.pop_back();
                 }
             }
             prev[i] = st.back().copied().unwrap_or(-1);
@@ -38,35 +48,35 @@ pub mod monotonic_stacks {
     }
 
     pub fn next_greater<T: PartialOrd>(a: &[T]) -> Vec<usize> {
-        monotonic_stack_next(a, |&a, &b| a > b)
+        monotonic_stack_next(a, |curr_value, next_value| next_value > curr_value)
     }
 
     pub fn next_greater_equal<T: PartialOrd>(a: &[T]) -> Vec<usize> {
-        monotonic_stack_next(a, |&a, &b| a >= b)
+        monotonic_stack_next(a, |curr_value, next_value| next_value >= curr_value)
     }
 
     pub fn next_smaller<T: PartialOrd>(a: &[T]) -> Vec<usize> {
-        monotonic_stack_next(a, |&a, &b| a < b)
+        monotonic_stack_next(a, |curr_value, next_value| next_value < curr_value)
     }
 
     pub fn next_smaller_equal<T: PartialOrd>(a: &[T]) -> Vec<usize> {
-        monotonic_stack_next(a, |&a, &b| a <= b)
+        monotonic_stack_next(a, |curr_value, next_value| next_value <= curr_value)
     }
 
     pub fn prev_greater<T: PartialOrd>(a: &[T]) -> Vec<isize> {
-        monotonic_stack_prev(a, |&a, &b| a > b)
+        monotonic_stack_prev(a, |curr_value, prev_value| prev_value > curr_value)
     }
 
     pub fn prev_greater_equal<T: PartialOrd>(a: &[T]) -> Vec<isize> {
-        monotonic_stack_prev(a, |&a, &b| a >= b)
+        monotonic_stack_prev(a, |curr_value, prev_value| prev_value >= curr_value)
     }
 
     pub fn prev_smaller<T: PartialOrd>(a: &[T]) -> Vec<isize> {
-        monotonic_stack_prev(a, |&a, &b| a < b)
+        monotonic_stack_prev(a, |curr_value, prev_value| prev_value < curr_value)
     }
 
     pub fn prev_smaller_equal<T: PartialOrd>(a: &[T]) -> Vec<isize> {
-        monotonic_stack_prev(a, |&a, &b| a <= b)
+        monotonic_stack_prev(a, |curr_value, prev_value| prev_value <= curr_value)
     }
 }
 use monotonic_stacks::*;
