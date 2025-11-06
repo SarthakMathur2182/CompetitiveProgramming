732870b368d5ab48590750a04527e0c44bed42bc
Sarthak Mathur <mathursarthak2182@gmail.com>
2025-11-06 23:02:26 +0530

    Updates in graph template + first commit of grid module

---
+++ Graph/Grid/grid.rs
@@ -0,0 +1,34 @@
+pub mod grid {
+    /// This is made from the perspective of traversing a 2-D array, so the direction and the difference might not make sense.
+    pub static DIR_GRID4: [(u8, isize, isize); 4] =
+        [(b'D', 1, 0), (b'R', 0, 1), (b'U', -1, 0), (b'L', 0, -1)];
+
+    pub static DIR_GRID8: [(isize, isize); 8] = [
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (-1, 1),
+        (-1, 0),
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+    ];
+
+    pub static DIR_GRID_DIAGONAL: [(isize, isize); 4] = [(1, 1), (-1, 1), (-1, -1), (1, -1)];
+
+    pub static DIR_CHESS_KNIGHT: [(isize, isize); 8] = [
+        (1, 2isize),
+        (-1, 2),
+        (-2, -1),
+        (-2, 1),
+        (1, -2),
+        (-1, -2),
+        (2, 1),
+        (2, -1),
+    ];
+
+    pub fn check_grid_bound(i: isize, j: isize, n: usize, m: usize) -> bool {
+        i >= 0 && i < n as isize && j >= 0 && j < m as isize
+    }
+}
+use grid::*;
--- Graph/graph.rs
+++ Graph/graph.rs
@@ -1,56 +1,97 @@
 pub mod graph {
     #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-    enum NodeVisitStatus {
+    pub enum NodeVisitStatus {
         NotVisited,
         Visiting,
         Visited,
     }
 
-    pub struct Graph {
-        pub n: usize,
-        pub vis: Vec<NodeVisitStatus>,
-        pub adj: Vec<Vec<u32>>,
+    #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+    pub enum EdgeType {
+        Unknown,
+        TreeEdge,
+        BackEdge,
+        ForwardEdge,
+        CrossEdge,
     }
 
-    impl Graph {
-        pub fn new(n: usize) -> Self {
+    #[derive(Clone, Eq, PartialEq)]
+    pub struct Edge<T: Clone + Eq + PartialEq + Default> {
+        pub u: u32,
+        pub v: u32,
+        pub w: T,
+    }
+
+    impl<T: Clone + Eq + PartialEq + Default> Edge<T> {
+        pub fn new(u: u32, v: u32, w: T) -> Self {
+            Self { u, v, w }
+        }
+    }
+
+    pub struct Graph<T: Clone + Eq + PartialEq + Default, const DIRECTED: bool> {
+        pub n: usize,
+        pub adj: Vec<Vec<u32>>,
+        pub edges: Vec<Edge<T>>,
+        pub node_visit_status: Vec<NodeVisitStatus>,
+        pub edge_type: Vec<EdgeType>,
+        // /// Take care of the parent of the root.
+        // pub parent: Vec<u32>,
+    }
+
+    impl<T: Clone + Eq + PartialEq + Default, const DIRECTED: bool> Graph<T, DIRECTED> {
+        pub fn new(n: usize, m: usize) -> Self {
             Self {
                 n,
-                vis: vec![NodeVisitStatus::NotVisited; n],
                 adj: vec![vec![]; n],
+                edges: Vec::with_capacity(m),
+                node_visit_status: vec![NodeVisitStatus::NotVisited; n],
+                edge_type: Vec::with_capacity(m),
+                // parent: vec![0; n],
             }
         }
 
-        pub fn add_edge(&mut self, u: usize, v: usize) {
-            self.adj[u].push(v as u32);
-            // self.adj[v].push(u as u32);
+        pub fn add_edge(&mut self, u: usize, v: usize, w: T) {
+            self.adj[u].push(self.edges.len() as u32);
+            if !DIRECTED {
+                self.adj[v].push(self.edges.len() as u32);
+            }
+            self.edges.push(Edge::new(u as u32, v as u32, w));
+            self.edge_type.push(EdgeType::Unknown);
         }
 
-        pub fn dfs(&mut self, u: usize) {
-            if self.vis[u] == NodeVisitStatus::Visited {
-                return;
-            }
-            assert!(self.vis[u] != NodeVisitStatus::Visiting);
-            self.vis[u] = NodeVisitStatus::Visiting;
-            // Pre-order process u
+        pub fn dfs(&mut self, u: usize, par: Option<usize>) {
+            assert!(self.node_visit_status[u] == NodeVisitStatus::NotVisited);
+            self.node_visit_status[u] = NodeVisitStatus::Visiting;
             for i in 0..self.adj[u].len() {
-                let v = self.adj[u][i] as usize;
-                match self.vis[v] {
+                let i = self.adj[u][i] as usize;
+                if !DIRECTED && self.edge_type[i] != EdgeType::Unknown {
+                    continue;
+                }
+                assert!(self.edge_type[i] == EdgeType::Unknown);
+
+                let v = (self.edges[i].u ^ self.edges[i].v) as usize ^ u;
+                match self.node_visit_status[v] {
                     NodeVisitStatus::NotVisited => {
                         // Process tree edge before traversal
-                        self.dfs(v);
+
+                        self.edge_type[i] = EdgeType::TreeEdge;
+                        // self.parent[v] = u as u32;
+                        self.dfs(v, Some(u));
+
                         // Process bottom up operations (related to backing from v to u)
                     }
                     NodeVisitStatus::Visiting => {
+                        self.edge_type[i] = EdgeType::BackEdge;
                         // Process back edge
                     }
                     NodeVisitStatus::Visited => {
-                        // Process cross edge
+                        // TODO: Process Forward edge and Cross edge for directed graph.
                         // assert!(false); // In case of undirected graph
                     }
                 }
             }
             // Post-order process u
+            self.node_visit_status[u] = NodeVisitStatus::Visited;
         }
     }
 
