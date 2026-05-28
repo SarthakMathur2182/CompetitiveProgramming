f4018de3e6a56de4a5e25f2eccb73acdf0062170
Sarthak Mathur <mathursarthak2182@gmail.com>
2026-05-28 15:57:55 +0530

    Modified ModInt (not tested)

--- Math/ModInt/mod_int.rs
+++ Math/ModInt/mod_int.rs
@@ -5,85 +5,64 @@
 /// You'll need the module [math.rs](https://github.com/SarthakMathur2182/CompetitiveProgramming/blob/main/CombinedRustModules/math.rs) to use the same.
 pub mod mod_int {
     use super::math::custom_math_traits::*;
-    use std::convert::TryInto;
+    use std::convert::{TryFrom, TryInto};
     use std::fmt::{Debug, Display, Error, Formatter};
-    use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+    use std::ops::{
+        Add, AddAssign, BitAnd, Div, DivAssign, Mul, MulAssign, Neg, Rem, ShrAssign, Sub, SubAssign,
+    };
 
-    pub mod modulos {
-        use std::fmt::{Debug, Display};
-        use std::ops::{Add, BitAnd, Mul, Rem, ShrAssign, Sub, SubAssign};
+    #[repr(u64)]
+    #[derive(Copy, Clone)]
+    #[allow(non_camel_case_types)]
+    pub enum MODS {
+        Mod_1e9_7 = 1_000_000_007,
+        Mod_1e9_9 = 1_000_000_009,
+        Mod_998 = 998_244_353,
+    }
 
-        #[repr(u64)]
-        #[derive(Copy, Clone)]
-        #[allow(non_camel_case_types)]
-        pub enum MOD {
-            Mod_1e9_7 = 1_000_000_007,
-            Mod_1e9_9 = 1_000_000_009,
-            Mod_998 = 998_244_353,
-        }
+    /// OpT denotes the operation type (mainly for multiplication)
+    pub trait Modulo {
+        type T: Copy
+            + Clone
+            + Ord // Keeping Ord for T in case we ever need it (but not for ModInt)
+            + PartialOrd // Also this is required in modular inverse (it is the exponent)
+            + BitAnd<Output = Self::T>
+            + ShrAssign
+            + Eq
+            + PartialEq
+            + Add<Output = Self::T>
+            + Sub<Output = Self::T>
+            + Default
+            + MultiplicativeIdentity
+            + From<u8>
+            + Display
+            + Debug;
 
-        /// OpT denotes the operation type (mainly for multiplication)
-        pub trait Modulo {
-            type T: Copy
-                + Ord
-                + PartialOrd
-                + Eq
-                + PartialEq
-                + Add<Output = Self::T>
-                + Sub<Output = Self::T>
-                + SubAssign
-                + Default
-                + super::super::math::custom_math_traits::MultiplicativeIdentity
-                + From<u8>
-                + BitAnd<Output = Self::T>
-                + ShrAssign
-                + Display
-                + Debug;
+        type OpT: Copy
+            + Clone
+            + Eq
+            + PartialEq
+            + Rem<Output = Self::OpT>
+            + Mul<Output = Self::OpT>
+            + From<Self::T>;
 
-            type OpT: Copy
-                + Ord
-                + PartialOrd
-                + Eq
-                + PartialEq
-                + From<Self::T>
-                + Add<Output = Self::OpT>
-                + Sub<Output = Self::OpT>
-                + SubAssign
-                + Rem<Output = Self::OpT>
-                + Mul<Output = Self::OpT>;
+        const MOD: Self::T;
 
-            const MOD: Self::T;
-
-            fn opt_to_t(val: Self::OpT) -> Self::T;
-        }
+        /// Not using [TryInto] to avoid overhead.
+        fn opt_to_t(val: Self::OpT) -> Self::T;
     }
 
     /// This struct does not type cast when adding, so make sure `2 * (MOD - 1) <= T::MAX`
-    #[derive(Eq, PartialEq)]
-    pub struct ModInt<M: modulos::Modulo> {
-        pub val: M::T,
-    }
+    #[derive(Eq, PartialEq, Copy, Clone)]
+    pub struct ModInt<M: Modulo>(M::T);
 
-    impl<M: modulos::Modulo> Clone for ModInt<M>
-    where
-        M::T: Copy,
-    {
-        fn clone(&self) -> Self {
-            *self
-        }
-    }
-
-    impl<M: modulos::Modulo> Copy for ModInt<M> where M::T: Copy {}
-
-    impl<M: modulos::Modulo> ModInt<M> {
-        pub fn from<U: Into<M::OpT>>(val: U) -> Self {
-            Self {
-                val: Self::norm(val),
-            }
+    impl<M: Modulo> ModInt<M> {
+        pub fn new<U: Into<M::OpT>>(val: U) -> Self {
+            Self(Self::normalize(val))
         }
 
         #[inline]
-        pub fn norm<U: Into<M::OpT>>(val: U) -> M::T {
+        pub fn normalize<U: Into<M::OpT>>(val: U) -> M::T {
             let rem = M::opt_to_t(val.into() % M::OpT::from(M::MOD));
             if rem < M::T::default() {
                 rem + M::MOD
@@ -93,18 +72,33 @@ pub mod mod_int {
         }
 
         #[inline]
+        /// Ideally this should be unsafe, but keeping it simple for now.
         pub fn sub_mod_if_big(val: M::T) -> M::T {
             if val >= M::MOD { val - M::MOD } else { val }
         }
     }
 
-    impl<M: modulos::Modulo> AddAssign for ModInt<M> {
-        fn add_assign(&mut self, rhs: Self) {
-            self.val = ModInt::<M>::sub_mod_if_big(self.val + rhs.val);
+    impl<M: Modulo, U: Into<M::OpT>> From<U> for ModInt<M> {
+        fn from(val: U) -> Self {
+            Self::new(val)
         }
     }
 
-    impl<M: modulos::Modulo> Add for ModInt<M> {
+    impl<M: Modulo, U: TryInto<M::OpT>> TryFrom<U> for ModInt<M> {
+        type Error = TryInto<M::OpT>::Error;
+
+        fn try_from(val: U) -> Result<Self, Self::Error> {
+            Self::new(val.try_into())
+        }
+    }
+
+    impl<M: Modulo> AddAssign for ModInt<M> {
+        fn add_assign(&mut self, rhs: Self) {
+            self.0 = ModInt::<M>::sub_mod_if_big(self.0 + rhs.0);
+        }
+    }
+
+    impl<M: Modulo> Add for ModInt<M> {
         type Output = ModInt<M>;
 
         fn add(self, rhs: Self) -> Self::Output {
@@ -114,31 +108,31 @@ pub mod mod_int {
         }
     }
 
-    impl<M: modulos::Modulo> Default for ModInt<M> {
+    impl<M: Modulo> Default for ModInt<M> {
         fn default() -> Self {
-            ModInt::from(M::T::default())
+            ModInt(M::T::default())
         }
     }
 
-    impl<M: modulos::Modulo> Neg for ModInt<M> {
+    impl<M: Modulo> Neg for ModInt<M> {
         type Output = ModInt<M>;
 
         fn neg(self) -> Self::Output {
-            if self.val == M::T::default() {
+            if self.0 == M::T::default() {
                 self
             } else {
-                ModInt::from(M::MOD - self.val)
+                ModInt(M::MOD - self.0)
             }
         }
     }
 
-    impl<M: modulos::Modulo> SubAssign for ModInt<M> {
+    impl<M: Modulo> SubAssign for ModInt<M> {
         fn sub_assign(&mut self, rhs: Self) {
             *self += -rhs;
         }
     }
 
-    impl<M: modulos::Modulo> Sub for ModInt<M> {
+    impl<M: Modulo> Sub for ModInt<M> {
         type Output = ModInt<M>;
 
         fn sub(self, rhs: Self) -> Self::Output {
@@ -148,13 +142,13 @@ pub mod mod_int {
         }
     }
 
-    impl<M: modulos::Modulo> MulAssign for ModInt<M> {
+    impl<M: Modulo> MulAssign for ModInt<M> {
         fn mul_assign(&mut self, rhs: Self) {
-            self.val = ModInt::<M>::norm(M::OpT::from(self.val) * M::OpT::from(rhs.val));
+            self.0 = ModInt::<M>::normalize(M::OpT::from(self.0) * M::OpT::from(rhs.0));
         }
     }
 
-    impl<M: modulos::Modulo> Mul for ModInt<M> {
+    impl<M: Modulo> Mul for ModInt<M> {
         type Output = ModInt<M>;
 
         fn mul(self, rhs: Self) -> Self::Output {
@@ -164,13 +158,13 @@ pub mod mod_int {
         }
     }
 
-    impl<M: modulos::Modulo> MultiplicativeIdentity for ModInt<M> {
+    impl<M: Modulo> MultiplicativeIdentity for ModInt<M> {
         fn one() -> Self {
-            ModInt::from(M::T::one())
+            ModInt(M::T::one())
         }
     }
 
-    impl<M: modulos::Modulo> MultiplicativeInverse for ModInt<M> {
+    impl<M: Modulo> MultiplicativeInverse for ModInt<M> {
         type Output = ModInt<M>;
 
         fn mul_inv(&self) -> Self::Output {
@@ -178,13 +172,13 @@ pub mod mod_int {
         }
     }
 
-    impl<M: modulos::Modulo> DivAssign for ModInt<M> {
+    impl<M: Modulo> DivAssign for ModInt<M> {
         fn div_assign(&mut self, rhs: Self) {
             *self *= rhs.mul_inv();
         }
     }
 
-    impl<M: modulos::Modulo> Div for ModInt<M> {
+    impl<M: Modulo> Div for ModInt<M> {
         type Output = ModInt<M>;
 
         fn div(self, rhs: Self) -> Self::Output {
@@ -194,37 +188,34 @@ pub mod mod_int {
         }
     }
 
-    impl<M: modulos::Modulo> Debug for ModInt<M> {
+    impl<M: Modulo> Debug for ModInt<M> {
         fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-            write!(f, "Mint({})", self.val)
+            write!(f, "Mint({})", self.0)
         }
     }
 
-    impl<M: modulos::Modulo> Display for ModInt<M> {
+    impl<M: Modulo> Display for ModInt<M> {
         fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-            write!(f, "{}", self.val)
+            write!(f, "{}", self.0)
         }
     }
 
     /// The factorials are present till `N-1` only, so initialize N accordingly.
-    pub struct Combinatorics<M: modulos::Modulo, const N: usize> {
+    pub struct Combinatorics<M: Modulo, const N: usize> {
         factorial: Vec<ModInt<M>>,
         inverse_factorial: Vec<ModInt<M>>,
     }
 
-    impl<M: modulos::Modulo, const N: usize> Combinatorics<M, N> {
-        pub fn new() -> Self
-        where
-            M::OpT: From<u32>,
-        {
+    impl<M: Modulo, const N: usize> Combinatorics<M, N> {
+        pub fn new() -> Self {
             let mut factorial = vec![ModInt::<M>::one(); N];
             for i in 1..N {
-                factorial[i] = factorial[i - 1] * ModInt::from(i as u32);
+                factorial[i] = factorial[i - 1] * ModInt::try_from(i);
             }
             let mut inverse_factorial = vec![ModInt::<M>::one(); N];
             inverse_factorial[N - 1] = factorial[N - 1].mul_inv();
             for i in (0..N - 1).rev() {
-                inverse_factorial[i] = inverse_factorial[i + 1] * ModInt::from(i as u32 + 1);
+                inverse_factorial[i] = inverse_factorial[i + 1] * ModInt::try_from(i + 1);
             }
 
             Self {
@@ -255,11 +246,18 @@ pub mod mod_int {
             I: TryInto<usize> + Copy,
             <I as TryInto<usize>>::Error: Debug,
         {
-            if n.try_into().is_err() || r.try_into().is_err() {
-                return ModInt::default();
-            }
-            let n = n.try_into().unwrap();
-            let r = r.try_into().unwrap();
+            let n = match n.try_into() {
+                Ok(n) => n,
+                Err(_) => {
+                    return ModInt::default();
+                }
+            };
+            let r = match r.try_into() {
+                Ok(r) => r,
+                Err(_) => {
+                    return ModInt::default();
+                }
+            };
             if r > n {
                 ModInt::default()
             } else {
@@ -273,11 +271,18 @@ pub mod mod_int {
             I: TryInto<usize> + Copy,
             <I as TryInto<usize>>::Error: Debug,
         {
-            if n.try_into().is_err() || r.try_into().is_err() {
-                return ModInt::default();
-            }
-            let n = n.try_into().unwrap();
-            let r = r.try_into().unwrap();
+            let n = match n.try_into() {
+                Ok(n) => n,
+                Err(_) => {
+                    return ModInt::default();
+                }
+            };
+            let r = match r.try_into() {
+                Ok(r) => r,
+                Err(_) => {
+                    return ModInt::default();
+                }
+            };
             if r > n {
                 ModInt::default()
             } else {
@@ -296,14 +301,48 @@ pub mod mod_int {
             assert!(n > 0, "Inverse of 0 does not exist!");
             self.inverse_factorial[n] * self.factorial[n - 1]
         }
+
+        pub fn c<I>(&self, n: I, r: I) -> ModInt<M>
+        where
+            I: TryInto<u32> + Copy,
+            <I as TryInto<u32>>::Error: Debug,
+        {
+            let n = match n.try_into() {
+                Ok(n) => n,
+                Err(_) => {
+                    return ModInt::default();
+                }
+            };
+            let mut r = match r.try_into() {
+                Ok(r) => r,
+                Err(_) => {
+                    return ModInt::default();
+                }
+            };
+
+            if r > n {
+                return ModInt::default();
+            }
+
+            if r > n - r {
+                r = n - r;
+            }
+
+            let mut ans = ModInt::default();
+            for i in 0..r {
+                ans *= ModInt::try_from(n - i);
+                ans /= self.inv(i + 1);
+            }
+            ans
+        }
     }
 }
 use mod_int::*;
 struct Mod1e9_7;
-impl modulos::Modulo for Mod1e9_7 {
+impl Modulo for Mod1e9_7 {
     type T = u32;
     type OpT = u64;
-    const MOD: Self::T = modulos::MOD::Mod_1e9_7 as u32;
+    const MOD: Self::T = MODS::Mod_1e9_7 as u32;
 
     fn opt_to_t(val: Self::OpT) -> Self::T {
         val as u32
